0172e697e66c3c7fc0f8f777e4a6e9ba4daeb21a
Christian Alexandersson <Chrale83@outlook.com>
2024-09-09 21:36:31 +0200

    Add project files.

---
+++ Labb1_C#/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+string inputString = "29535123p48723487597645723645";
+char tempChar = ' ';
+int startIndex = 0;
+int stopIndex = 0;
+string tempSubString = "";
+string lastSubstring = "";
+int totalSum = 0;
+
+for (int i = 0; i < inputString.Length; i++)
+{
+	if (!char.IsDigit(inputString[i]))
+	{
+		break;
+	}
+
+	if (i == 0)
+	{
+	tempChar = inputString[i];
+	startIndex = inputString.IndexOf(inputString[i]);
+	}
+
+	if (i > 0 && tempChar == inputString[i])
+	{
+		stopIndex = i;
+		tempSubString = inputString.Substring(startIndex, i + 1);
+		lastSubstring = inputString.Substring(i+1);
+
+		totalSum += int.Parse(tempSubString);
+    }
+
+
+
+}
+    Console.WriteLine(tempSubString + " " + lastSubstring);
+	Console.WriteLine(totalSum);
+
+
