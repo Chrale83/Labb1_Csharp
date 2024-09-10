17a1f1624d9866f90346eaeb08da9118741c662a
Christian Alexandersson <Chrale83@outlook.com>
2024-09-10 06:11:04 +0200

    Påbörjat labben: plockar ut värdet en gång

--- Labb1_C#/Program.cs
+++ Labb1_C#/Program.cs
@@ -1,39 +1,61 @@
 ﻿
 
-string inputString = "29535123p48723487597645723645";
+string inputString = "295235123p48723487597645723645";
 char tempChar = ' ';
 int startIndex = 0;
 int stopIndex = 0;
-string tempSubString = "";
+string firstSubstring = "";
+string ValueSubString = "";
 string lastSubstring = "";
-int totalSum = 0;
+long totalSum = 0;
+
 
 for (int i = 0; i < inputString.Length; i++)
 {
-	if (!char.IsDigit(inputString[i]))
-	{
-		break;
-	}
-
-	if (i == 0)
-	{
-	tempChar = inputString[i];
-	startIndex = inputString.IndexOf(inputString[i]);
-	}
-
-	if (i > 0 && tempChar == inputString[i])
-	{
-		stopIndex = i;
-		tempSubString = inputString.Substring(startIndex, i + 1);
-		lastSubstring = inputString.Substring(i+1);
-
-		totalSum += int.Parse(tempSubString);
+    if (!char.IsDigit(inputString[i]))
+    {
+        break;
     }
 
+    if (i == 0)
+    {
+        tempChar = inputString[i];
+        startIndex = i;
+        
+    }
 
-
+    if (i > 0 && tempChar == inputString[i]) 
+    {
+        stopIndex = i;
+        firstSubstring = inputString.Substring(0, startIndex);
+        ValueSubString = inputString.Substring(startIndex, (stopIndex - startIndex + 1));
+        lastSubstring = inputString.Substring(i + 1);
+        totalSum += long.Parse(ValueSubString);
+        break;
+    }
 }
-    Console.WriteLine(tempSubString + " " + lastSubstring);
-	Console.WriteLine(totalSum);
+
+    
+
+
+
+Console.WriteLine($"Start index är: {startIndex}");
+Console.WriteLine($"tempchar är: {tempChar}");
+Console.WriteLine($"stopIndex är; {stopIndex}");
+Console.WriteLine($"operation är; {stopIndex - startIndex + 1}");
+
+
+Console.WriteLine($"correctsubstring är:   {ValueSubString}");
+
+
+Console.Write(firstSubstring);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write(ValueSubString);
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write(lastSubstring);
+
+
+
+
 
 
